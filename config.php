50287c562294fa25f66bed62352cc3f5c47e13bf
Oscar Perez La Madrid <oepmadrid@ucm.es>
2015-06-27 21:30:33 +0200

    Google Oauth
    Autentificacion con Google.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//Estos datos se onsiguen en : console.developers.google.com
+
+$oauth2_client_id = {CLIENT ID}
+$oauth2_secret = {oauth2_secret}
+$oauth2_redirect = {redireccion despues de la autentificacion}
+?>
